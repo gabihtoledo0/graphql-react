7bd043c7da200698963bd8899a71c6c8766b999c
Gabriela  Toledo Prado <gabriela.prado@youse.com.br>
2020-07-27 21:18:42 -0300

    mutation execution started

--- src/containers/AuthorCreator.js
+++ src/containers/AuthorCreator.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { useFormik } from "formik";
 import * as yup from "yup";
+import * as mutations from "../api/mutations"
+import { useMutation } from "react-apollo"
 
 function AuthorCreator() {
-  const { getFieldProps, touched, errors, isValid } = useFormik({
+  const [ addAuthor ] = useMutation(mutations.ADD_AUTHOR)
+  const { getFieldProps, touched, errors, isValid, handleSubmit } = useFormik({
     initialValues: {
       authorName: "",
     },
     validationSchema: yup.object({
       authorName: yup.string().required("O nome do autor é obrigatório"),
     }),
+    onSubmit: (values, actions) => {
+      addAuthor({
+        variables: {
+          name: values.authorName
+        }
+      })
+      actions.setValues({ authorName: ""}, false)
+    }
   });
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <input
           type="text"
