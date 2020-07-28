90fab5021aafa7ed11a0a9f19ed93b23b8c007ca
Gabriela  Toledo Prado <gabriela.prado@youse.com.br>
2020-07-28 20:13:37 -0300

    adjust errors

--- src/containers/BookCreator.js
+++ src/containers/BookCreator.js
@@ -3,16 +3,22 @@ import { useFormik } from "formik";
 import * as yup from "yup";
 
 function BookCreator() {
-  const { getFieldProps, touched, errors } = useFormik({
+  const { getFieldProps, touched, errors, isValid, handleSubmit } = useFormik({
     initialValues: {
       bookTitle: "",
     },
     validationSchema: yup.object({
       bookTitle: yup.string().required("o título do livro é obrigatório"),
     }),
+    onSubmit: (values, actions) => {
+      console.log({
+        bookTitle: values.bookTitle,
+        authorId: values.authorId
+      })
+    }
   });
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <input
           type="text"
@@ -24,6 +30,7 @@ function BookCreator() {
           <small>{errors.bookTitle}</small>
         ) : null}
       </div>
+      <button type="submit" disabled={!isValid}>Adicionar</button>
     </form>
   );
 }
