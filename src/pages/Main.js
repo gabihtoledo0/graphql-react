3eff9607e52ad1d16f2aa8734a1d5bce86cf7ef6
Gabriela  Toledo Prado <gabriela.prado@youse.com.br>
2020-07-28 19:52:12 -0300

    input for book title

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import BookList from "../containers/BookList";
 import AuthorCreator from "../containers/AuthorCreator";
+import BookCreator from "../containers/BookCreator";
 
 function Main() {
   return (
     <>
       <BookList />
       <AuthorCreator />
+      <BookCreator />
     </>
   );
 }
